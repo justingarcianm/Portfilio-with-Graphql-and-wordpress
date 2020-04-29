c9fe68eb4f4e72d5aebe0b9495d650d8943b0625
justingarcianm <justingarcianm@gmail.com>
2020-04-29 17:47:54 -0600

    inital packages and pages set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Project from './projects/Project'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
+const client = new ApolloClient({
+  uri:'http://justins-portfolio.local/graphql'
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ApolloProvider client={client}>
+     <BrowserRouter>
+     <Header/>
+       <div className="container">
+         <Route exact path="/" component={Home}/>
+         <Route path="/about" component={About}/>
+         <Route path="/projects/:slug" component={Project}/>
+       </div>
+     <Footer/>
+     </BrowserRouter>
+   </ApolloProvider>
   );
 }
 

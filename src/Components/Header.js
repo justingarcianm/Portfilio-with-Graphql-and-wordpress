4be97c9ffe5baab009a3fa72595f0c3b15cbca47
justingarcianm <justingarcianm@gmail.com>
2020-06-02 17:18:26 -0600

    navbar functionality set

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import './header.css'
 
 const Header = () => {
     return(
@@ -7,6 +8,23 @@ const Header = () => {
             <NavLink to="/">
             Navbar
             </NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+                    </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/about">About</NavLink>
+                    </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://github.com/justingarcianm"><i className="fab fa-github"></i></a>
+                    </li>
+            </ul>
+            </div>
         </nav> 
     )
 }

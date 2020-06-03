6e1c30500c731fa15d21f236cbf7d482e4dc3f70
justingarcianm <justingarcianm@gmail.com>
2020-06-03 17:53:28 -0600

    set arrow functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Projects from '../projects/Projects'
 import { Link } from 'react-router-dom'
 import './home.css'
 
 const Home = () => {
+
+    const [ text, setText ] = useState(false)
+
     return(
         <>
         <div className="text-center" id="home">
@@ -11,9 +14,16 @@ const Home = () => {
             <div className="row">
                 <div className="col my-auto">
                     <h1>Hi, my name is Justin</h1>
-                    <h3>A web developer</h3>
+                    <h3>A web developer</h3>  
+
+                    <div className={`textBlock ${text ? 'showText' : 'hideText'}`} >
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat consequat mauris. Ornare suspendisse sed nisi lacus sed viverra tellus in. Sagittis orci a scelerisque purus semper eget duis at. Odio euismod lacinia at quis risus sed vulputate.</p>
                     <Link to='/about'>Read more about me!</Link>
+                    </div>
+                    <i 
+                    className={`fas fa-chevron-down arrow ${text ? 'show' : ''}`}
+                    onClick={() => setText(!text)}
+                    ></i>
                 </div>
             </div>
             </div>            
